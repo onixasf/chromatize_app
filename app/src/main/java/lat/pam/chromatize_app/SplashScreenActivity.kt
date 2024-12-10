e1a3981e7a0cf027c320dfa6d2d29f6405bd1fef
onixasf <onixashafa@gmail.com>
2024-12-10 20:05:04 +0700

    update3

---
+++ app/src/main/java/lat/pam/chromatize_app/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package lat.pam.chromatize_app
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Menampilkan splash screen selama 2 detik
+        Handler().postDelayed({
+            // Pindah ke MainActivity setelah splash screen
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Menutup SplashScreenActivity
+        }, 2000) // Delay 2000 ms
+    }
+}
